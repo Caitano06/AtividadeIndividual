9a42e538e1d9aa0c0276792c1402b0ded60a55c1
Lucas Caitano <lucacaitano2001@gmail.com>
2025-03-26 20:10:02 -0300

    adicionado os blocos de try-catch nos controllers

--- AvaliacaoPrimeiraFase/controllers/tarefaController.js
+++ AvaliacaoPrimeiraFase/controllers/tarefaController.js
@@ -1,43 +1,57 @@
-
 const criarTarefa = async (req, res) => {
-  const { titulo, alunoId, disciplinasIds } = req.body;
-  const concluida = false;
+  try {
+    const { titulo, alunoId, disciplinasIds } = req.body;
+    const concluida = false;
 
-  const novaTarefa = new Tarefa({
-    titulo,
-    aluno: alunoId,
-    concluida,
-    disciplinas: disciplinasIds,
-  });
+    const novaTarefa = new Tarefa({
+      titulo,
+      aluno: alunoId,
+      concluida,
+      disciplinas: disciplinasIds,
+    });
 
-  await novaTarefa.save();
+    await novaTarefa.save();
 
-  res.json({
-    message: "Tarefa criada com sucesso!",
-    tarefa: novaTarefa,
-  });
+    res.json({
+      message: "Tarefa criada com sucesso!",
+      tarefa: novaTarefa,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao criar tarefa", error });
+  }
 };
 
 const obterTodasTarefas = async (req, res) => {
-  const tarefas = await Tarefa.find().populate("aluno").populate("disciplinas");
-  res.json(tarefas);
+  try {
+    const tarefas = await Tarefa.find().populate("aluno").populate("disciplinas");
+    res.json(tarefas);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao obter tarefas", error });
+  }
 };
 
 const deletarTarefa = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await Tarefa.deleteOne({ _id: id });
-  res.json({ message: "Tarefa removida com sucesso!" });
+    await Tarefa.deleteOne({ _id: id });
+    res.json({ message: "Tarefa removida com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao deletar tarefa", error });
+  }
 };
 
 const editarTarefa = async (req, res) => {
-  const { id } = req.params;
-  const { titulo, concluida } = req.body;
+  try {
+    const { id } = req.params;
+    const { titulo, concluida } = req.body;
 
-  let tarefa = await Tarefa.findByIdAndUpdate(id, { titulo, concluida });
-  res.status(200).json({
-    message: "Tarefa atualizada com sucesso!",
-    tarefa,
-  });
+    let tarefa = await Tarefa.findByIdAndUpdate(id, { titulo, concluida });
+    res.status(200).json({
+      message: "Tarefa atualizada com sucesso!",
+      tarefa,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao editar tarefa", error });
+  }
 };
-
--- AvaliacaoPrimeiraFase/controllers/turmaController.js
+++ AvaliacaoPrimeiraFase/controllers/turmaController.js
@@ -1,42 +1,55 @@
-
-
 const criarTurma = async (req, res) => {
-  const { nome, alunosIds, professorId } = req.body;
+  try {
+    const { nome, alunosIds, professorId } = req.body;
 
-  const novaTurma = new Turma({
-    nome,
-    alunos: alunosIds,
-    professor: professorId,
-  });
+    const novaTurma = new Turma({
+      nome,
+      alunos: alunosIds,
+      professor: professorId,
+    });
 
-  await novaTurma.save();
+    await novaTurma.save();
 
-  res.json({
-    message: "Turma criada com sucesso!",
-    turma: novaTurma,
-  });
+    res.json({
+      message: "Turma criada com sucesso!",
+      turma: novaTurma,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao criar turma", error });
+  }
 };
 
 const obterTodasTurmas = async (req, res) => {
-  const turmas = await Turma.find().populate('alunos professor');
-  res.json(turmas);
+  try {
+    const turmas = await Turma.find().populate('alunos professor');
+    res.json(turmas);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao obter turmas", error });
+  }
 };
 
 const deletarTurma = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await Turma.deleteOne({ _id: id });
-  res.json({ message: "Turma removida com sucesso!" });
+    await Turma.deleteOne({ _id: id });
+    res.json({ message: "Turma removida com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao deletar turma", error });
+  }
 };
 
 const editarTurma = async (req, res) => {
-  const { id } = req.params;
-  const { nome, alunosIds, professorId } = req.body;
+  try {
+    const { id } = req.params;
+    const { nome, alunosIds, professorId } = req.body;
 
-  let turma = await Turma.findByIdAndUpdate(id, { nome, alunos: alunosIds, professor: professorId });
-  res.status(200).json({
-    message: "Turma atualizada com sucesso!",
-    turma,
-  });
+    let turma = await Turma.findByIdAndUpdate(id, { nome, alunos: alunosIds, professor: professorId });
+    res.status(200).json({
+      message: "Turma atualizada com sucesso!",
+      turma,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao editar turma", error });
+  }
 };
-
